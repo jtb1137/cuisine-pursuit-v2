194f499cea3d0dde43b5b1a56e3b736769afa78b
jtb1137 <jtb1137@gmail.com>
2018-03-06 10:05:20 -0500

    build cities controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,50 @@
 class CitiesController < ApplicationController
+    before_action :set_city, only: [:show, :edit, :update, :delete]
+
+    def index
+        @cities = City.all
+    end
+
+    def show
+    end
+
+    def new
+        @city = City.new(city_params)
+    end
+    
+    def create
+        @city = City.new(city_params)
+
+        if @city.save?
+            redirect_to city_path(@city)
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @city.update(city_params)
+            redirect_to city_path(@city)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @city.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def city_params
+        params.require(:city).permit(:name, :state)
+    end
+
+    def set_city
+        @city = City.find(params[:id])
+    end
 end
